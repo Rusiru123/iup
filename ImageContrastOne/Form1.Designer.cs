bffbb4df28d6b82fd7bf5bb89062c61b98a89933
rusiru1995 <rusiru@smallaxe.tech>
2020-04-22 19:54:54 +0530

    added the iup contrast image

---
+++ ImageContrastOne/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ImageContrastOne
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(76, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(277, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 57);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(76, 128);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(701, 266);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "JPG|*.jpg|BMP|*.bmp|PNG|*.png|GIF|*.gif";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPG|*.jpg|BMP|*.bmp|PNG|*.png|GIF|*.gif";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(515, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 57);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "GrayScale";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 410);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Contrast";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(196, 410);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(417, 56);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(631, 419);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 517);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Image Console";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ImageContrastOne/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace ImageContrastOne
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap bitmapNow;
+        Image fileOne;
+        Boolean opened = false;
+        float contrast = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult diaR = openFileDialog1.ShowDialog();
+
+            if(diaR == DialogResult.OK)
+            {
+                fileOne = Image.FromFile(openFileDialog1.FileName);
+                bitmapNow = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = fileOne;
+                opened = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult diaR = saveFileDialog1.ShowDialog();
+
+            if(diaR == DialogResult.OK)
+            {
+                if(opened)
+                {
+                    if (saveFileDialog1.FileName.Substring(saveFileDialog1.FileName.Length - 3).ToLower() == "bmp") {
+                        fileOne.Save(saveFileDialog1.FileName, ImageFormat. Bmp);
+                    }
+
+                    if (saveFileDialog1.FileName.Substring(saveFileDialog1.FileName.Length - 3).ToLower() == "jpg")
+                    {
+                        fileOne.Save(saveFileDialog1.FileName, ImageFormat.Jpeg);
+                    }
+
+                    if (saveFileDialog1.FileName.Substring(saveFileDialog1.FileName.Length - 3).ToLower() == "png")
+                    {
+                        fileOne.Save(saveFileDialog1.FileName, ImageFormat.Png);
+                    }
+
+                    if (saveFileDialog1.FileName.Substring(saveFileDialog1.FileName.Length - 3).ToLower() == "gif")
+                    {
+                        fileOne.Save(saveFileDialog1.FileName, ImageFormat.Gif);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please Open an image first!!");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for(int i = 0;i < bitmapNow.Width; i++)
+            {
+                for(int j=0; j< bitmapNow.Height;j++)
+                {
+                    Color freshColor = bitmapNow.GetPixel(i,j);
+
+                    int grayScale = (int)((freshColor.R * .3) + (freshColor.G * .59) + (freshColor.B * .11));
+
+                    Color modifiedColor = Color.FromArgb(grayScale, grayScale, grayScale);
+
+                    bitmapNow.SetPixel(i,j,modifiedColor);
+                }
+            }
+
+            pictureBox1.Image = bitmapNow;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label2.Text = trackBar1.Value.ToString();
+
+            contrast = 0.04f * trackBar1.Value;
+
+            Bitmap bmp = new Bitmap(bitmapNow.Width, bitmapNow.Height);
+
+            Graphics gr = Graphics.FromImage(bmp);
+            ImageAttributes iatt = new ImageAttributes();
+
+            ColorMatrix cmx = new ColorMatrix(new float[][] { new float[] { contrast,0f,0f,0f,0f},
+                new float[] { 0f,contrast,0f,0f,0f},
+                new float[] { 0f,0f,contrast,0f,0f},
+                new float[] { 0f,0f,0f,1f,0f},
+
+                new float[] { 0.001f,0.001f,0.001f,0f,1f}
+            });
+
+            iatt.SetColorMatrix(cmx);
+
+            gr.DrawImage(bitmapNow,new Rectangle(0,0,bitmapNow.Width,bitmapNow.Height), 0, 0,bitmapNow.Width,bitmapNow.Height,GraphicsUnit.Pixel,iatt);
+            gr.Dispose();
+            iatt.Dispose();
+            pictureBox1.Image = bmp;
+        }
+    }
+}
